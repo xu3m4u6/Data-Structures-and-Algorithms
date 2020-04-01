fc6d95943a8bb6acc5385b25b6d646fa8d4fc2ac
Judy Li <xu3m4u6@gmail.com>
2020-04-01 23:31:40 +0800

    ReverseString.java

--- DataStructure-Array/ReverseString.java
+++ DataStructure-Array/ReverseString.java
@@ -5,23 +5,6 @@ class ReverseString{
 	static String reverse(String input){
 
 		char[] sToCharArray = input.toCharArray();
-		int left = 0;
-		int right = sToCharArray.length-1;
-
-
-		for (left=0; left < right; left++, right--){
-			char tmp = sToCharArray[left];
-			sToCharArray[left] = sToCharArray[right];
-			sToCharArray[right] = tmp;
-		}
-
-		String result = new String(sToCharArray);
-		return result;
-	}
-
-	static String reverse2(String input){
-
-		char[] sToCharArray = input.toCharArray();
 		int elements = sToCharArray.length;
 
 		char[] backward = new char[elements];
@@ -35,6 +18,22 @@ class ReverseString{
 		return result;
 	}
 
+	static String reverse2(String input){
+
+		char[] sToCharArray = input.toCharArray();
+		int left = 0;
+		int right = sToCharArray.length-1;
+
+
+		for (left=0; left < right; left++, right--){
+			char tmp = sToCharArray[left];
+			sToCharArray[left] = sToCharArray[right];
+			sToCharArray[right] = tmp;
+		}
+
+		String result = new String(sToCharArray);
+		return result;
+	}
 
 	public static void main (String[] args){
 		
