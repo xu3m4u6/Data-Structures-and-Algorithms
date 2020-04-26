8cc1109e9d1a9bed457b75ff4c5ab0dbfcb6477d
Judy Li <xu3m4u6@gmail.com>
2020-04-26 15:52:30 +0800

    Stack Implementation using Linked List

---
+++ DataStructure-Stack&Queue/MyStackL.java
@@ -0,0 +1,87 @@
+
+public class MyStackL {
+  Node top;
+  Node bottom;
+  int length = 0;
+
+  public class Node {
+    String value;
+    Node next;
+
+    Node (String data){
+      value = data;
+      next = null;
+    }
+  }
+
+  public String peek() {
+    if(length == 0){
+      return null;
+    }
+    return top.value;
+  }
+
+  public MyStackL push(String value){
+    Node newNode = new Node(value);
+    if (length == 0){
+      bottom = newNode;
+      top = newNode;
+    }else{
+      Node holdingPointer = top;
+      top = newNode;
+      top.next = holdingPointer;
+    }
+    length++;
+    return this;
+  }
+  
+  public MyStackL pop(){
+    if(length == 0){
+      return null;
+    }
+    if(top == bottom){
+      top = null;
+      bottom = null;
+      length--;
+      return this;
+    }
+
+    // Node holdingPointer = top;
+    top = top.next;
+    length--;
+    return this;
+  }
+
+ 
+  public Boolean isEmpty(){
+    if(length == 0){
+      return true;
+    }
+    return false;
+  }
+
+  public static void main(String[] args){
+
+    MyStackL obj = new MyStackL();
+    System.out.println("The stack is empty? " + obj.isEmpty());
+
+    obj.push("google");
+    System.out.println(obj.peek());
+    obj.push("udemy");
+    System.out.println(obj.peek());
+    obj.push("discord");
+    System.out.println(obj.peek());
+
+    System.out.println("The stack is empty? " + obj.isEmpty());
+
+    obj.pop();
+    System.out.println(obj.peek());
+    obj.pop();
+    System.out.println(obj.peek());
+    obj.pop();
+    System.out.println(obj.peek());
+    System.out.println("The stack is empty? " + obj.isEmpty());
+  }
+
+}
+
