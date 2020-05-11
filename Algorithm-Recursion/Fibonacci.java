c663c79a7c93ff50fb8e6cebafd647e659b4730e
Judy Li <xu3m4u6@gmail.com>
2020-05-11 22:42:27 +0800

    Fibonacci

--- Algorithm-Recursion/Fibonacci.java
+++ Algorithm-Recursion/Fibonacci.java
@@ -1,7 +1,7 @@
 class Fibonacci {
 // Given a number N return the index value of the Fibonacci sequence, where the sequence is:
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
-// the pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 → 2+3
+// the pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 → 2+3=5
 //For example: fibonacciRecursive(6) should return 8
 
 	static int fibonacciIterative(int n){
