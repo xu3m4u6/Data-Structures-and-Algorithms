52b16694f12d44bbc306ac9f80a5aa608ed581c7
Judy Li <xu3m4u6@gmail.com>
2020-05-22 16:56:31 +0800

    Merge Sort

--- Algorithm-Sorting/MyMergeSort.java
+++ Algorithm-Sorting/MyMergeSort.java
@@ -1,25 +1,55 @@
 class MyMergeSort {
-	static int[] mergeSort(int[] array) {
-		if (array.length === 1) {
-		    return array;
+	public static void merge(int[] left_arr,int[] right_arr, int[] arr,int left_size, int right_size){
+
+		int i=0,l=0,r = 0;
+      //The while loops check the conditions for merging
+		while(l<left_size && r<right_size){
+		  
+		  if(left_arr[l]<right_arr[r]){
+		      arr[i++] = left_arr[l++];
 		  }
-		  // Split Array in into right and left
-
-		  return merge(
-		    mergeSort(left),
-		    mergeSort(right)
-		  )
+		  else{
+		      arr[i++] = right_arr[r++];
+		  }
+		}
+		while(l<left_size){
+		  arr[i++] = left_arr[l++];
+		}
+		while(r<right_size){
+		arr[i++] = right_arr[r++];
+		}
 	}
-	
-	static int[] merge(in[] left, int[] right){
 
+  	public static void mergeSort(int [] arr, int len){
+	  	if (len < 2){return;}
+	  	int mid = len / 2;
+	  	int [] left_arr = new int[mid];
+	    int [] right_arr = new int[len-mid];
+	      
+	    //Dividing array into two and copying into two separate arrays
+		int k = 0;
+		for(int i = 0;i<len;++i){
+		  if(i<mid){
+		      left_arr[i] = arr[i];
+		  }
+		  else{
+		      right_arr[k] = arr[i];
+		      k = k+1;
+		  }
+		}
+    // Recursively calling the function to divide the subarrays further
+		mergeSort(left_arr,mid);
+		mergeSort(right_arr,len-mid);
+		// Calling the merge method on each subdivision
+		merge(left_arr,right_arr,arr,mid,len-mid);
 	}
 
 
-	public static void main(String[] args) {
+  	public static void main(String[] args) {
 		int[] numbers = {99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0};
-		int[] answer = mergeSort(numbers);
-		for (int num: answer)
-			System.out.print(num + " ");
+		mergeSort(numbers,numbers.length);
+		for(int i =0; i< numbers.length;++i){
+		System.out.print(numbers[i]+ " ");
+		}
 	}
 }
