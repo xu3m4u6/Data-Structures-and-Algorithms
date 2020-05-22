01b967a1e73037e665d4ce5765478acc491b532a
Judy Li <xu3m4u6@gmail.com>
2020-05-22 19:34:44 +0800

    Merge Sort

--- Algorithm-Sorting/MyMergeSort.java
+++ Algorithm-Sorting/MyMergeSort.java
@@ -1,53 +1,73 @@
 class MyMergeSort {
-	public static void merge(int[] left_arr,int[] right_arr, int[] arr,int left_size, int right_size){
-
-		int i=0,l=0,r = 0;
-      //The while loops check the conditions for merging
-		while(l<left_size && r<right_size){
-		  
-		  if(left_arr[l]<right_arr[r]){
-		      arr[i++] = left_arr[l++];
-		  }
-		  else{
-		      arr[i++] = right_arr[r++];
-		  }
-		}
-		while(l<left_size){
-		  arr[i++] = left_arr[l++];
-		}
-		while(r<right_size){
-		arr[i++] = right_arr[r++];
-		}
-	}
-
-  	public static void mergeSort(int [] arr, int len){
-	  	if (len < 2){return;}
-	  	int mid = len / 2;
-	  	int [] left_arr = new int[mid];
-	    int [] right_arr = new int[len-mid];
-	      
-	    //Dividing array into two and copying into two separate arrays
-		int k = 0;
-		for(int i = 0;i<len;++i){
-		  if(i<mid){
-		      left_arr[i] = arr[i];
-		  }
-		  else{
-		      right_arr[k] = arr[i];
-		      k = k+1;
-		  }
-		}
-    // Recursively calling the function to divide the subarrays further
-		mergeSort(left_arr,mid);
-		mergeSort(right_arr,len-mid);
-		// Calling the merge method on each subdivision
-		merge(left_arr,right_arr,arr,mid,len-mid);
-	}
-
+	public static void merge(int arr[], int l, int m, int r) { 
+        // Find sizes of two subarrays to be merged 
+        int n1 = m - l + 1; 
+        int n2 = r - m; 
+  
+        /* Create temp arrays */
+        int L[] = new int [n1]; 
+        int R[] = new int [n2]; 
+  
+        /*Copy data to temp arrays*/
+        for (int i=0; i<n1; ++i) 
+            L[i] = arr[l + i]; 
+        for (int j=0; j<n2; ++j) 
+            R[j] = arr[m + 1+ j]; 
+  
+  
+        /* Merge the temp arrays */
+  
+        // Initial indexes of first and second subarrays 
+        int i = 0, j = 0; 
+  
+        // Initial index of merged subarry array 
+        int k = l; 
+        while (i < n1 && j < n2) { 
+            if (L[i] <= R[j]) { 
+                arr[k] = L[i]; 
+                i++; 
+            } 
+            else{ 
+                arr[k] = R[j]; 
+                j++; 
+            } 
+            k++; 
+        } 
+  
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) { 
+            arr[k] = L[i]; 
+            i++; 
+            k++; 
+        } 
+  
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) { 
+            arr[k] = R[j]; 
+            j++; 
+            k++; 
+        } 
+    } 
+  
+    // Main function that sorts arr[l..r] using 
+    // merge() 
+    public static void mergeSort(int arr[], int l, int r) { 
+        if (l < r) { 
+            // Find the middle point 
+            int m = (l+r)/2; 
+  
+            // Sort first and second halves 
+            mergeSort(arr, l, m); 
+            mergeSort(arr , m+1, r); 
+  
+            // Merge the sorted halves 
+            merge(arr, l, m, r); 
+        } 
+    } 
 
   	public static void main(String[] args) {
 		int[] numbers = {99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0};
-		mergeSort(numbers,numbers.length);
+		mergeSort(numbers, 0, numbers.length-1);
 		for(int i =0; i< numbers.length;++i){
 		System.out.print(numbers[i]+ " ");
 		}
