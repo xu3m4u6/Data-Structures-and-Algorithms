7a8476732a081ed7d8fa7de4f75ed141b6ac99d0
Judy Li <xu3m4u6@gmail.com>
2020-06-19 09:17:58 +0800

    Searching

---
+++ Algorithm-Searching/MyTree.java
@@ -0,0 +1,217 @@
+import java.util.*;
+
+public class MyTree {
+
+	private static class Node {
+		private int value;
+		private Node left;
+		private Node right;
+
+		public Node(int value) {
+			this.value = value;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+	Node root;
+
+	public MyTree(){
+		this.root = null;
+	}
+
+	public boolean insert(int value) {
+		Node newNode = new Node(value);
+		if( root == null){
+			root = newNode;
+			return true;
+		}else{
+			Node current = this.root;
+			while(true) {
+				// Left
+				if(value < current.value) {
+					if(current.left == null){
+						current.left = newNode;
+						return true;
+					}
+					current = current.left;
+
+				}else if (value > current.value){
+				// Right
+					if(current.right == null){
+						current.right = newNode;
+						return true;
+					}
+					current = current.right;
+
+				}else if(value == current.value){
+					System.out.println("Duplicate value! Cannot insert " + value + " to the tree.");
+					return false;
+				}
+			}
+		}
+	}
+
+	public boolean lookup(int value) {
+		if(root == null){
+			return false;
+		}
+		Node current = root;
+		while(current != null){
+			if(value < current.value){
+				current = current.left;
+			} else if(value > current.value) {
+				current = current.right;
+			} else if(value == current.value) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean remove(int value) {
+
+	    Node currentNode = this.root;
+	    Node parentNode = null;
+
+	    while(currentNode != null){
+	    	if(value < currentNode.value){
+	    		parentNode = currentNode;
+	    		currentNode = currentNode.left;
+	    	} else if(value > currentNode.value){
+	    		parentNode = currentNode;
+	    		currentNode = currentNode.right;
+	    	} else if (currentNode.value == value) {
+	    	//We have a match, get to work!
+	        
+	        //Option 1: No right child: 
+	        	if (currentNode.right == null) {
+	        		if (parentNode == null) {
+		        		this.root = currentNode.left;
+		        	} else {
+		        	//if parent > current value, make current left child a child of parent
+		        		if(currentNode.value < parentNode.value) {
+		        			parentNode.left = currentNode.left;
+		        	
+		        	//if parent < current value, make left child a right child of parent
+		        		} else if(currentNode.value > parentNode.value) {
+		        			parentNode.right = currentNode.left;
+		        		}
+	        		}
+	        
+	        //Option 2: Right child which doesnt have a left child
+	        	} else if (currentNode.right.left == null) {
+
+		        	currentNode.right.left = currentNode.left;
+		        	if(parentNode == null) {
+		        		this.root = currentNode.right;
+		        	} else {
+		        	//if parent > current, make right child of the left the parent
+		        		if(currentNode.value < parentNode.value) {
+		        			parentNode.left = currentNode.right;
+		        	
+		        	//if parent < current, make right child a right child of the parent
+		        		} else if (currentNode.value > parentNode.value) {
+		        			parentNode.right = currentNode.right;
+		        		}
+		        	}
+	        
+	        //Option 3: Right child that has a left child
+	        	} else {
+	        		//find the Right child's left most child
+	        		Node leftmost = currentNode.right.left;
+	        		Node leftmostParent = currentNode.right;
+		        	while(leftmost.left != null) {
+		        		leftmostParent = leftmost;
+		            	leftmost = leftmost.left;
+		            }
+	          
+	        		//Parent's left subtree is now leftmost's right subtree
+		            leftmostParent.left = leftmost.right;
+		            leftmost.left = currentNode.left;
+		            leftmost.right = currentNode.right;
+
+		            if(parentNode == null) {
+		            	this.root = leftmost;
+		            } else {
+		            	if(currentNode.value < parentNode.value) {
+		            		parentNode.left = leftmost;
+		            	} else if(currentNode.value > parentNode.value) {
+		            		parentNode.right = leftmost;
+		            	}
+		            }
+		        }
+	        	return true;
+	        }
+	    }
+	    return false;
+	}
+
+	// BFS
+	public ArrayList breadthFirstSearch() {
+		Node currentNode = this.root;
+		ArrayList<Integer> result = new ArrayList<>();
+		Queue<Node> memorize = new LinkedList<>();
+		memorize.offer(currentNode);
+
+	    while (!memorize.isEmpty()){
+	    	currentNode = memorize.poll();
+	    	result.add(currentNode.value);
+
+	    	if(currentNode.left != null){
+	    		memorize.offer(currentNode.left);
+	    	}
+	    	if(currentNode.right != null){
+	    		memorize.offer(currentNode.right);
+	    	}
+	    }
+	    return result;
+	}
+
+	// BFS recursive
+	public ArrayList breadthFirstSearchRecursive(Queue<Node> memorize, ArrayList<Integer> result) {
+		
+		if(memorize.isEmpty())
+			return result;
+
+		Node currentNode = memorize.poll();
+		result.add(currentNode.value);
+
+    	if(currentNode.left != null){
+    		memorize.offer(currentNode.left);
+    	}
+    	if(currentNode.right != null){
+    		memorize.offer(currentNode.right);
+    	}
+	    
+	    return this.breadthFirstSearchRecursive(memorize, result);
+	}
+
+
+	public static void main(String[] args) {
+		MyTree tree = new MyTree();
+		tree.insert(9);
+		tree.insert(4);
+		tree.insert(6);
+		tree.insert(20);
+		tree.insert(170);
+		tree.insert(15);
+		tree.insert(1);
+		
+		// BFS iterative
+		System.out.println(tree.breadthFirstSearch());
+
+		// BFS recursive
+		ArrayList<Integer> result = new ArrayList<>();
+		Queue<Node> memorize = new LinkedList<>();
+		memorize.offer(tree.root);
+		System.out.println(tree.breadthFirstSearchRecursive(memorize, result));
+	}
+}
+
+
+// Tree Structure
+// //     9
+// //  4     20
+// //1  6  15  170
+
